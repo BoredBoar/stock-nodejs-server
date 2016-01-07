f48337c7250935f4f73aec16f9c750400c1ca925
BoredBoar <christopher.lee.williams@gmail.com>
2016-01-07 14:06:21 -0500

    Update server.js
    Can only instantiate server once <facepalm>

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(PORT, function () {
+server.listen(PORT, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
